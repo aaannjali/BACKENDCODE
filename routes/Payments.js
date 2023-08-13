3587951fecb8b3321e376ac2c5f4ac80829ea426
Anjali Sharma <121468990+aaannjali@users.noreply.github.com>
2023-08-13 13:27:21 +0530

    dd

--- routes/Payments.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../controllers/Payments")
-const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
-router.post("/capturePayment", auth, isStudent, capturePayment)
-
-
-module.exports = router;
